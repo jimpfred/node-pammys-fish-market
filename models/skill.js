1f33e6a54ea5a4fcf2d4b1683e6b7b2ecf3a7f3e
jimpfred <jimpfred@yahoo.com>
2021-06-18 06:20:28 -0700

    forgot to upload latest readme

--- models/skill.js
+++ models/skill.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const skillSchema = new Schema({
     name: String,
-    address: String
+    address: String,
+    longitude: Number,
+    latitude: Number
 }, { timestamps: true});
 
 module.exports = mongoose.model('Skill', skillSchema);
